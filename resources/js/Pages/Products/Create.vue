2322149ec152b8921980ceb51b5a10988eb7fd98
Andrea Forlin <andrea.forlin@gmail.com>
2023-03-10 00:42:42 +0100

    fix: dev: doppio caricamento

--- resources/js/Pages/Products/Create.vue
+++ resources/js/Pages/Products/Create.vue
@@ -34,7 +34,7 @@ const onSubmit = () => {
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
         <Form
-          @submit="onSubmit"
+          @submit.prevent="onSubmit"
           :form="form"
           :title="$t('product.create')"
         ></Form>
